1a3889e6b63dfac02a3941cd8555d6a02b3db6a1
Vadim <svanovivan@gmail.com.com>
2019-12-12 08:40:02 +0200

    Added task_1

--- js/script.js
+++ js/script.js
@@ -1,23 +1,9 @@
 const logItems = function(array) {
-  console.log(array);
   for (i = 0; i < array.length; i += 1) {
-    console.log(array[i]);
+    console.log(`${i + 1}  ${array[i]}`);
   }
 };
-
-logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
-// console.log(logItems);
-
-/*
- * Вызовы функции для проверки работоспособности твоей реализации.
-
-
 
 logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
-
-const logItems = function(array) {
-  for (let i = 0; i < logItems.length; i += 1) {
-    console.log('Logging clients: ', clients[i]);
-  }
-};
- */
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+// logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
