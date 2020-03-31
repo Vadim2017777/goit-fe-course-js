37fe6fc716bf9b38e1305cd5504d611ca4b3eebd
Vadim <svanovivan@gmail.com>
2020-03-31 17:12:30 +0300

    hw-10

--- hw-10/src/index.js
+++ hw-10/src/index.js
@@ -1,9 +1,46 @@
 import './styles.css';
 
-const input = document.querySelector('.js-switch-input');
-console.dir(input);
-input.addEventListener('click', handleClick);
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
 
-function handleClick() {
-  document.querySelector('body').classList.toggle('dark-theme');
+const refs = {
+  body: document.querySelector('body'),
+  input:document.querySelector('.js-switch-input')
+}
+
+const BaseTheme = () => {
+  if(localStorage.getItem('Theme') == Theme.DARK){
+    refs.input.checked = true;
+    refs.classList.add(localStorage.getItem('Theme'));
+  };
+
+const BaseTheme = document
+  .querySelector('body')
+  .classList.add(localStorage.getItem('Theme'));
+
+const input = document.querySelector('.js-switch-input');
+
+if (localStorage.getItem('Theme') == Theme.DARK) {
+  input.checked = true;
+
+  console.log('HELLO');
+}
+input.addEventListener('change', handleClick);
+
+function handleClick(e) {
+  console.log(e.target.checked);
+
+  if (e.target.checked === true) {
+    document.querySelector('body').classList.remove('light-theme');
+    document.querySelector('body').classList.add('dark-theme');
+    localStorage.removeItem('Theme');
+    localStorage.setItem('Theme', Theme.DARK);
+  } else if (e.target.checked === false) {
+    document.querySelector('body').classList.remove('dark-theme');
+    document.querySelector('body').classList.add('light-theme');
+    localStorage.removeItem('Theme');
+    localStorage.setItem('Theme', Theme.LIGHT);
+  }
 }
