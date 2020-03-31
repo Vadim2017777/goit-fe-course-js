1b6c0f2c9d297bf1c08b99ddc7dcb028e040157f
Vadim <svanovivan@gmail.com.com>
2020-03-31 21:38:16 +0300

    hw-10

--- hw-10/src/index.js
+++ hw-10/src/index.js
@@ -1,9 +1,28 @@
 import './styles.css';
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+const baseTheme = document
+  .querySelector('body')
+  .classList.add(localStorage.getItem('theme'));
 
 const input = document.querySelector('.js-switch-input');
-console.dir(input);
 input.addEventListener('click', handleClick);
 
+console.log(localStorage);
+
 function handleClick() {
-  document.querySelector('body').classList.toggle('dark-theme');
+  console.log(localStorage);
+  if (localStorage.getItem('theme') === Theme.LIGHT || 'null') {
+    document.querySelector('body').classList.add(Theme.DARK);
+    document.querySelector('body').classList.remove(Theme.LIGHT);
+    localStorage.removeItem('theme');
+
+    console.log(localStorage.getItem('theme'));
+  } else {
+    document.querySelector('body').classList.remove(Theme.DARK);
+  }
 }
+
+console.log(localStorage.getItem('theme'));
