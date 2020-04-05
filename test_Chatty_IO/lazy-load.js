bb55f5654decdc012a034f73f69312f64a67edbd
Vadim <svanovivan@gmail.com.com>
2020-04-05 22:30:06 +0300

    Add IO

--- test_Chatty_IO/lazy-load.js
+++ test_Chatty_IO/lazy-load.js
@@ -1,1 +1,27 @@
-"use strict";
+'use strict';
+
+const lazyLoad = target => {
+  const options = {
+    rootMargin: '-40px 0px',
+    threshold: 0.01,
+  };
+
+  const io = new IntersectionObserver((entreis, observer) => {
+    entreis.forEach(entry => {
+      if (entry.isIntersecting) {
+        const img = entry.target;
+
+        const src = img.dataset.lazy;
+        img.setAttribute('src', src);
+        observer.disconnect();
+      }
+    });
+  }, options);
+  io.observe(target);
+};
+
+const images = document.querySelectorAll('.gallery__image');
+
+images.forEach(image => {
+  lazyLoad(image);
+});
--- test_Chatty_IO/script.js
+++ test_Chatty_IO/script.js
@@ -1,42 +1,42 @@
-import galleryItems from "./gallery-items.js";
+import galleryItems from './gallery-items.js';
 
-const gallery = document.querySelector(".js-gallery");
-gallery.addEventListener("click", handleClickImage);
+const gallery = document.querySelector('.js-gallery');
+gallery.addEventListener('click', handleClickImage);
 
 const closeModalBtn = document.querySelector(`[data-action="close-lightbox"]`);
-const lightbox = document.querySelector(".js-lightbox");
-closeModalBtn.addEventListener("click", handleCloseModal);
+const lightbox = document.querySelector('.js-lightbox');
+closeModalBtn.addEventListener('click', handleCloseModal);
 
-const wideImage = document.querySelector(".lightbox__image");
-const overlay = document.querySelector(".lightbox__content");
-overlay.addEventListener("click", handleCloseModal);
+const wideImage = document.querySelector('.lightbox__image');
+const overlay = document.querySelector('.lightbox__content');
+overlay.addEventListener('click', handleCloseModal);
 
 const nextImgBtn = document.querySelector(`[data-action="next-image"]`);
-nextImgBtn.addEventListener("click", nextImage);
+nextImgBtn.addEventListener('click', nextImage);
 
 const prevtImgBtn = document.querySelector(`[data-action="prev-image"]`);
-prevtImgBtn.addEventListener("click", prevImage);
+prevtImgBtn.addEventListener('click', prevImage);
 
 insertGallery(galleryItems);
 
 function insertGallery(galleryItems) {
-  galleryItems.forEach((item) => {
-    let li = document.createElement("li");
+  galleryItems.forEach(item => {
+    let li = document.createElement('li');
 
     li.insertAdjacentHTML(
-      "afterbegin",
+      'afterbegin',
       `<a
           class="gallery__link"
           href="${item.original}"
         >
           <img
             class="gallery__image"
-            src="${item.preview}"
-            data-lazy-srcset="${item.lazy}"
+            src=""
+            data-lazy="${item.lazy}"
             data-source="${item.original}"
             alt="${item.description}"
           />
-        </a>`
+        </a>`,
     );
     gallery.append(li);
   });
@@ -44,30 +44,30 @@ function insertGallery(galleryItems) {
 
 function handleClickImage(e) {
   e.preventDefault();
-  window.addEventListener("keydown", handleKeyPress);
+  window.addEventListener('keydown', handleKeyPress);
   wideImage.src = e.target.dataset.source;
   wideImage.alt = e.target.alt;
-  lightbox.classList.add("is-open");
+  lightbox.classList.add('is-open');
 }
 
 function handleCloseModal(e) {
   if (e.target != e.currentTarget) {
     return;
   }
-  window.removeEventListener("keydown", handleKeyPress);
-  lightbox.classList.remove("is-open");
-  wideImage.src = "";
-  wideImage.alt = "";
+  window.removeEventListener('keydown', handleKeyPress);
+  lightbox.classList.remove('is-open');
+  wideImage.src = '';
+  wideImage.alt = '';
 }
 
 function handleKeyPress(e) {
-  if (e.code != "Escape") {
+  if (e.code != 'Escape') {
     return;
   }
 
-  lightbox.classList.remove("is-open");
-  wideImage.src = "";
-  wideImage.alt = "";
+  lightbox.classList.remove('is-open');
+  wideImage.src = '';
+  wideImage.alt = '';
 }
 
 function nextImage() {
@@ -99,3 +99,8 @@ function findImage(gallery) {
   });
   return ind;
 }
+
+// const lazyLoad = target => {};
+// const images = document.querySelectorAll('.gallery__image');
+
+// console.log(images);
